8151dc035b925c59c7fb2f8a41476ae30bee4c3c
mayank-always <140394120+mayank-always@users.noreply.github.com>
2024-05-04 10:26:36 -0400

    Added Descriptive Comments

--- USpopulation.py
+++ USpopulation.py
@@ -1,11 +1,11 @@
-#######################
+
 # Import libraries
 import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
 
-#######################
+
 # Page configuration
 st.set_page_config(
     page_title="US Population Dashboard",
